1ea1c4e0318918a6a6e5de33e1ba089c906eeda1
viveksabharwal <42807639+viveksabharwal@users.noreply.github.com>
2024-10-15 17:51:34 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // Treat all JS code as newer version. 
+
+console.log(3
+    +3
+) // code readability should be high.
+
+let name= "vivek";
+let age = 28;
+let isLoggedIn = true;
+
+//number ==> 2^53 (range)
+//bigint (for bigger numbers)
+// string ==> "" --> assembly of characters.
+//boolean ==> true/false
+//null ==> standalone value
+//undefined ==> kinda placeholder for a value which is not defined yet.
+//symbol ==> unique
+//object
+
+console.log(typeof name); //to display the type of.
+console.log(typeof null); // type ==> object.
+console.log(typeof undefined); // type ==> undefined.
+
