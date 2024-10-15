cbf03064dcda4821f861f040e57a1c6bebca4152
viveksabharwal <42807639+viveksabharwal@users.noreply.github.com>
2024-10-15 18:58:09 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,51 @@
+let score = 33;
+console.log(typeof score);
+
+let A = "33"
+console.log("type of A", typeof A);
+let valueInNumberA = Number(A);
+console.log("valueInNumberA", valueInNumberA);
+
+let B = "33abc"
+console.log("type of B", typeof B);
+let valueInNumberB = Number(B);
+console.log("valueInNumberB",valueInNumberB);
+
+let C = "null"
+console.log("type of C", typeof C);
+let valueInNumberC = Number(C);
+console.log("valueInNumberC", valueInNumberC);
+
+let D = "undefined"
+console.log("type of D", typeof D);
+let valueInNumberD = Number(D);
+console.log("valueInNumberD", valueInNumberD);
+
+let E = "true"
+console.log("type of E", typeof E);
+let valueInNumberE = Number(E);
+console.log("valueInNumberE", valueInNumberE);
+
+//String
+
+let F = 1235;
+console.log("type of F", typeof F);
+let valueInStringE = String(E);
+console.log("valueInStringE", valueInStringE);
+
+//Boolean
+let G = ""
+console.log("type of G", typeof G);
+let valueInBooleanG = Boolean(G);
+console.log("valueInBooleanG", valueInBooleanG);
+
+let H = 0
+console.log("type of H", typeof H);
+let valueInBooleanH = Boolean(H);
+console.log("valueInBooleanH", valueInBooleanH);
+
+let I = "vivek"
+console.log("type of I", typeof I);
+let valueInBooleanI = Boolean(I);
+console.log("valueInBooleanI", valueInBooleanI);
+
