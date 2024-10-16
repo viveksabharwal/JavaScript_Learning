e561ab428fe29887cf0e09fb7a1361876f162657
viveksabharwal <42807639+viveksabharwal@users.noreply.github.com>
2024-10-16 09:54:04 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,3 +1,5 @@
+// **********************************conversion******************************************
+/*
 let score = 33;
 console.log(typeof score);
 
@@ -48,4 +50,42 @@ let I = "vivek"
 console.log("type of I", typeof I);
 let valueInBooleanI = Boolean(I);
 console.log("valueInBooleanI", valueInBooleanI);
+*/
+// ************************************ Operations ******************************************************
 
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " vivek"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+//  console.log("1" + 2);
+//  console.log(1 + "2");
+// console.log("1" + 2 + 2); //first value is string it will consider whole value as string and display likewise
+// console.log(1 + 2 + "2"); //first value is num it will firts perfom the sum and then add the the string along with the output
+// // console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
